11bb46850aa8f12a3b11607aabe70f548c2846a2
ibrokhim <mr017programmer@gmail.com>
2022-01-18 07:01:11 +0500

    first-commit

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import useTranslation from "next-translate/useTranslation";
+import Link from 'next/link';
 
 const About = () => {
     const { t, lang } = useTranslation("common");
 
-    console.log('lang', lang);
+    // console.log('lang', lang);
 
     return (
         <div>
             <h1>{t("about-page")}</h1>
+            <Link href={"/"}>{t("home")}</Link>
         </div>
     )
 }
